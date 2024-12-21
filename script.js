a696a6456e9d26305a1e91ee4108bad5e825be27
isidro <alconcci22@gmail.com>
2024-12-20 22:34:25 -0600

    cargador

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+
+setTimeout(() => {
+    const loader = document.querySelector('.loader');
+    loader.style.display = 'none'; // Oculta completamente el cargador
+}, 10000); // 10 segundos (alineado con tu animación CSS)
+
+
